5f77a14a579d352550fea51e7d1969f9c51c4a31
Luscha <matteo.roggia@gmail.com>
2022-03-20 17:40:45 +0100

    refactor: backend running on port 8080

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,8 @@ import collections from './routes/collections';
 import probes from './routes/probes';
 import cors from 'cors';
 
+import { PORT, HOST } from './config';
+
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -10,8 +12,8 @@ app.use(cors())
 app.use('/collections', collections);
 app.use('/', probes);
 
-app.listen(3000, () =>
+app.listen({ port: PORT, host: HOST }, () =>
   console.log(`
-🚀 Server ready at: http://localhost:3000
+🚀 Server ready at: ${PORT}:${HOST}
 ⭐️ See sample requests: http://pris.ly/e/ts/rest-express#3-using-the-rest-api`),
 )

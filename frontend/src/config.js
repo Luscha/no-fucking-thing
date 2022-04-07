7c9a4fdd38c320a0dda975393cdf4019708a15b0
Luscha <matteo.roggia@gmail.com>
2022-04-07 19:13:38 +0200

    fix: env variables with VUE_APP_ prefix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,9 @@
 import {contractAdress} from "@/contract/address";
 
-export const NETWORK_URL = process.env.NETWORK_URL || 'http://localhost:1317';
-export const CHAIN_ID = process.env.CHAIN_ID || 'localterra';
+export const NETWORK_URL = process.env.VUE_APP_NETWORK_URL || 'http://localhost:1317';
+export const CHAIN_ID = process.env.VUE_APP_CHAIN_ID || 'localterra';
 
-export const MARKETPLACE_ADDRESS = process.env.MARKETPLACE_ADDRESS || contractAdress(CHAIN_ID, "marketplace");
-export const GEN0_ADDRESS = process.env.GEN0_ADDRESS || contractAdress(CHAIN_ID, "gen0");
+export const MARKETPLACE_ADDRESS = process.env.VUE_APP_MARKETPLACE_ADDRESS || contractAdress(CHAIN_ID, "marketplace");
+export const GEN0_ADDRESS = process.env.VUE_APP_GEN0_ADDRESS || contractAdress(CHAIN_ID, "gen0");
 
-export const BACKEND_REST_API_ENDPOINT = process.env.BACKEND_REST_API_ENDPOINT || 'http://localhost:3000'
+export const BACKEND_REST_API_ENDPOINT = process.env.VUE_APP_BACKEND_REST_API_ENDPOINT || 'http://localhost:3000'
--- frontend/src/contract/lcd.js
+++ frontend/src/contract/lcd.js
@@ -1,8 +1,9 @@
 import { LCDClient } from '@terra-money/terra.js'
 import {NETWORK_URL, CHAIN_ID} from '@/config.js'
 
-export const getLCD = () => (new LCDClient({
+export const getLCD = () => {
+  return new LCDClient({
     URL: NETWORK_URL,
     chainID: CHAIN_ID,
     // gasPrices: { uusd: 0.15 }
-  }))
+  })}

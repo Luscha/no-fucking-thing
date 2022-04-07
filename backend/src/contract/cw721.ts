754654f9e4171ad5d421fd8f33901f3c0c2fde45
Luscha <matteo.roggia@gmail.com>
2022-04-07 20:37:37 +0200

    fix: backend CW721Info

--- backend/src/contract/cw721.ts
+++ backend/src/contract/cw721.ts
@@ -2,6 +2,5 @@ import { query } from "@/contract/query"
 
 export const CW721Info = async (address): Promise<any>=> {
     const info = await query(address, { contract_info: {} });
-    const minter = await query(address,  { minter: {} });
-    return {...info, minter: minter.minter};
+    return info;
 }

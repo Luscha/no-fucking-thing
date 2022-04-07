6409116f5fbf5e3753538e45f73478950e24ad5e
Luscha <matteo.roggia@gmail.com>
2022-04-07 19:21:39 +0200

    debug logs

--- frontend/src/contract/lcd.js
+++ frontend/src/contract/lcd.js
@@ -2,6 +2,9 @@ import { LCDClient } from '@terra-money/terra.js'
 import {NETWORK_URL, CHAIN_ID} from '@/config.js'
 
 export const getLCD = () => {
+  console.log(NETWORK_URL)
+  console.log(process.env.NETWORK_URL)
+  console.log(CHAIN_ID)
   return new LCDClient({
     URL: NETWORK_URL,
     chainID: CHAIN_ID,
